cf482fa0ebb4082086caba2f0a418b5c838677e0
Reinier Dantas <50921411+reinierdantas@users.noreply.github.com>
2020-01-08 09:48:31 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ $('.arrowDown a[href^="#"], .menu-nav a[href^="#"]').click(function(e){
   },1000);      
 });
 
-//voltar ao topo
+//voltar ao topo ao clicar no botão back to top
 $('.home, .backBtn, .lang').click(function(e){
     e.preventDefault();
 $('html,body').animate({
@@ -17,7 +17,7 @@ $('html,body').animate({
     }, 1000);
 });
 
-//Aparecer o botão ao descer
+//Aparecer o botão back to top ao descer
 
 $(window).scroll(function(){
     if($(this).scrollTop() > 1000){
@@ -27,8 +27,29 @@ $(window).scroll(function(){
     }
 });
 
-//Menu mobile
+//Menu mobile responsivo
 $('.mobile-btn').click(function(){
     $(this).toggleClass('actived');
     $('.menu-mobile').toggleClass('actived');
+});
+
+
+//Animação ao descer o scroll
+var $target = $('[data-anime="scroll"]'),
+    animationClass = 'animate',
+    offset = $(window).height() * 3/4; 
+
+function animeScroll() {
+    var documentTop = $(window).scrollTop();
+    $target.each(function() {
+        var itemTop = $(this).offset().top;
+        if(documentTop >  itemTop - offset) {
+            $(this).addClass(animationClass);
+        } else {
+            $(this).removeClass(animationClass);
+        }
+    });
+}
+$(document).scroll(function() {
+    animeScroll();
 });
