4692f635ae235f76af8dd08eea2de6d7bf35617a
Reinier Dantas <50921411+reinierdantas@users.noreply.github.com>
2020-01-08 18:48:49 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -53,3 +53,12 @@ function animeScroll() {
 $(document).scroll(function() {
     animeScroll();
 });
+
+
+//Mudar homepage de acordo com a linguagem do browse
+const browseLang = navigator.language;
+if(browseLang = "en-US"){
+    window.document.setHomePage('https://reinierdantas.github.io/indexEn');
+} else{
+    window.document.setHomePage('https://reinierdantas.github.io');
+}
